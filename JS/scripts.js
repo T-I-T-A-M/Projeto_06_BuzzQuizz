fbb3ee1718fa27bfa9cc4d307ecfe55c09ce0c24
Dorigo <luizdorigo01@gmail.com>
2022-04-21 22:04:38 -0300

    ajustes-em-geral

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,27 +1,33 @@
 console.log ("tamo aqui")
 
-const TITTLEMINSIZE = 20
-const TITTLEMAXSIZE = 65
-const QUESTIONMINSIZE = 3
-const LEVELMINSIZE = 2
-const API = "https://mock-api.driven.com.br/api/v6/buzzquizz"
+const TITTLEMINSIZE = 20;
+const TITTLEMAXSIZE = 65;
+const QUESTIONMINSIZE = 3;
+const LEVELMINSIZE = 2;
+const API = "https://mock-api.driven.com.br/api/v6/buzzquizz";
 
 // const promise = axios.get(`${API}/quizzes`)
 
 function verifyQuizzCreation (){
 
-    const quizzTittle = document.querySelector(".quizz-tittle").value
-    const quizzURL = document.querySelector(".quizz-URL-image").value
-    const quizzQuestions = Number(document.querySelector(".quizz-questions").value)
-    const quizzLevels = Number(document.querySelector(".quizz-levels").value)
+    const quizzTittle = document.querySelector(".quizz-tittle").value;
+    const quizzURL = document.querySelector(".quizz-URL-image").value;
+    const quizzQuestions = Number(document.querySelector(".quizz-questions").value);
+    const quizzLevels = Number(document.querySelector(".quizz-levels").value);
 
 
-    if ( (quizzTittle.length >= TITTLEMINSIZE) && (quizzTittle.length <= 65) && (quizzURL.)) {
-       return alert ("você NAO é cabecao")
+    if ( (quizzTittle.length >= TITTLEMINSIZE) && (quizzTittle.length <= 65) && (quizzURL)) {
+       return alert ("você NAO é cabecao");
     }
-    return alert ("O TITULO DEVE CONTER ENTRE 20 E 65 CARACTERES")
+    return alert ("O TITULO DEVE CONTER ENTRE 20 E 65 CARACTERES");
 
 }
+
+function createQuizz(){
+    const page1 = document.querySelector(".page1").classList.add("hidden");
+    const page3 = document.querySelector(".page3").classList.remove("hidden");
+}
+
 
 
 
